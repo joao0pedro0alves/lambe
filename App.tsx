c2a3785aa6b9459393b8fc2c75bef8c1583145b7
João Alves <joao.alves@ecosis.com.br>
2022-12-12 09:13:06 -0300

    Start project, create theme and load custom fonts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, StatusBar, View} from "react-native"
+import {SafeAreaProvider} from "react-native-safe-area-context"
+
+import {
+    useFonts,
+    Roboto_400Regular,
+    Roboto_700Bold,
+    Roboto_900Black,
+} from "@expo-google-fonts/roboto"
+
+import {
+    Philosopher_400Regular,
+    Philosopher_700Bold,
+    Philosopher_400Regular_Italic,
+} from "@expo-google-fonts/philosopher"
+
+import {THEME} from "./src/theme"
+
+import {Home} from "./src/screens/Home"
+import {Loading} from "./src/components/Loading"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [fontsLoaded] = useFonts({
+        Roboto_400Regular,
+        Roboto_700Bold,
+        Roboto_900Black,
+        Philosopher_400Regular,
+        Philosopher_700Bold,
+        Philosopher_400Regular_Italic,
+    })
+
+    return (
+        <SafeAreaProvider>
+            <View style={styles.container}>
+                <StatusBar
+                    barStyle="light-content"
+                    backgroundColor="transparent"
+                    translucent
+                />
+
+                {fontsLoaded ? <Home /> : <Loading />}
+            </View>
+        </SafeAreaProvider>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: THEME.COLORS.BACKGROUND_PAPER,
+    },
+})
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from "react-native"
+
+// import {styles} from "./styles"
+
+export function Home() {
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}

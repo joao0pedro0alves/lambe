2adbf8006f1445e1fd3919a78cbe8de944f524ce
João Alves <joao.alves@ecosis.com.br>
2022-12-13 15:48:12 -0300

    Feat: Add user data on Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,36 @@
 import {View, Image, Text} from "react-native"
+import {Gravatar} from "react-native-gravatar"
 
 import icon from "../../assets/imgs/icon.png"
+import {useAuth} from "../../hooks/useAuth"
 
 import {styles} from "./styles"
 
 export function Header() {
+
+    const {currentUser} = useAuth()
+
     return (
         <View style={styles.container}>
             <View style={styles.toolbar}>
                 <Image source={icon} style={styles.logo} />
-
                 <Text style={styles.title}>Lambe Lambe</Text>
+
+                {currentUser && (
+                    <View style={styles.profile}>
+                        <View>
+                            <Text style={styles.hello}>Bem vindo,</Text>
+                            <Text style={styles.username}>{currentUser?.name}</Text>
+                        </View>
+                        <Gravatar
+                            style={styles.avatar}
+                            options={{
+                                email: currentUser?.email,
+                                secure: true,
+                            }}
+                        />
+                    </View>
+                )}
             </View>
         </View>
     )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -23,4 +23,27 @@ export const styles = StyleSheet.create({
         fontSize: THEME.FONT_SIZE.LG,
         height: 30,
     },
+
+    profile: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    hello: {
+        color: THEME.COLORS.CAPTION_400,
+        fontFamily: THEME.FONT_FAMILY.SANS.REGULAR,
+        fontSize: THEME.FONT_SIZE.XS,
+    },
+    username: {
+        color: THEME.COLORS.CAPTION_900,
+        fontFamily: THEME.FONT_FAMILY.SANS.BOLD,
+        fontSize: THEME.FONT_SIZE.XS,
+    },
+    avatar: {
+        height: 40,
+        width: 40,
+        borderRadius: 20,
+        marginLeft: THEME.SPACING.MD
+    }
 })
